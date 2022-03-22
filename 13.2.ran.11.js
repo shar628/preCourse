4bf9d5390dec3433dc66607a5a1b3f28d39e60b0
shar628 <shar628628@gmail.com>
2022-03-22 17:44:33 +0200

    commit name

---
+++ 13.2.ran.11.js
@@ -0,0 +1,28 @@
+/*
+
+const timeOfDay = ['Morning', 'Evening','afternoon', 'night',];
+let foo = `good ${timeOfDay[3]}`;
+console.log(foo);
+
+function hello( arg1,arg2) {
+    let foo2 = `${arg1}${arg2}`
+    return foo2
+}
+let foo3 = hello('30','!!');
+console.log(foo3);
+
+for (let i = 6; i >2 ; i-- ) {
+console.log(i);}
+
+for ( let i = 0 ; i < 12; i++) {
+    console.log (i);
+};
+*/
+let i = 24;
+let n =8 ;
+while (i < 30 || n > 0) {
+    console.log('n' + n);
+console.log('i' + i);
+i++;
+n--;
+}
